b80834d6680ad98485429c0a8470673c359c0756
Shunsuke Kimura <kimushun1101@gmail.com>
2020-05-24 05:23:32 +0000

    edit for cui version

--- 1_install_settings.sh
+++ 1_install_settings.sh
@@ -3,25 +3,12 @@ cd `dirname $0`
 
 sudo apt update
 
-xdg-open https://www.google.com/chrome/
-xdg-open https://code.visualstudio.com/docs/?dv=linux64_deb
-xdg-open https://slack.com/intl/ja-jp/downloads/linux
-
-sudo apt install -y vim terminator xsel
+sudo apt install -y vim
 mkdir ~/.vimbackup
-
-mkdir -p ~/.config/terminator
-cp -f terminator_config ~/.config/terminator/config
 
 mv ~/setup-ubuntu18 ~/.setup
 ln -s .setup/vimrc ~/.vimrc
 ln -s .setup/gvimrc ~/.gvimrc
 ln -s .setup/bash_aliases ~/.bash_aliases
-ln -s .setup/Xmodmap ~/.Xmodmap
-echo "xmodmap ~/.Xmodmap" >> ~/.bashrc
 
 source ~/.bashrc
-gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
-
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface clock-show-seconds true
