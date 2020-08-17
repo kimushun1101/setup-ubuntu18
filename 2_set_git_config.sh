56c119e43b834ea6692c23b079bc814308ca1cca
Shunsuke Kimura <kimushun1101@gmail.com>
2020-08-17 20:13:31 +0900

    confirm to change user name and email

--- 2_set_git_config.sh
+++ 2_set_git_config.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+read -p "Did you change user.name and user.email? (y/N): " yn
+case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+
 git config --global user.name "Shunsuke Kimura"
 git config --global user.email "kimushun1101@gmail.com"
 
@@ -10,5 +13,5 @@ fi
 source ~/.bashrc
 cat ~/.ssh/id_rsa.pub | pbcopy
 xdg-open https://github.com/settings/ssh
-read -p "Upload public key, then HIT ENTER: "
+read -p "Upload public key, then HIT ENTER: \n you can use 'cat ~/.ssh/id_rsa.pub | pbcopy' " continue
 ssh -T git@github.com
